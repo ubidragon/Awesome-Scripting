707fafc85eee486256a8d97e78c1170dce7f90a7
Ubidragon <ubidragon@protonmail.com>
2020-04-05 17:48:42 +0200

    exclude loopback interface

--- networks.sh
+++ networks.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#Idea de este script a S4vitar
+#Idea of this script thanks S4vitar
 #https://pastebin.com/yGvk0Ve8
 
-#Color de terminal
+#terminal colour
 #https://robologs.net/2016/03/31/como-colorear-el-output-de-la-terminal-en-linux/
 
 OUTPUT=""
@@ -12,21 +12,22 @@ INTERFAZ=""
 for interfaz in $(ip a | grep "mtu " | awk '{print $2}' )
 do
 	INTERFAZ=$(echo $interfaz | sed 's/://')
-	
-    for ips in $(ip address show $INTERFAZ | grep "inet " | awk '{print $2}')
-    do
-        OUTPUT+="\e[32m$interfaz\e[0m"
-        OUTPUT+=" \t"
-        OUTPUT+=$ips
-        OUTPUT+="\n"
-    done
-    
+	if [ "$INTERFAZ" != "lo" ];	
+	then
+	    for ips in $(ip address show $INTERFAZ | grep "inet " | awk '{print $2}')
+	    do
+		OUTPUT+="\e[32m$interfaz\e[0m"
+		OUTPUT+=" \t"
+		OUTPUT+=$ips
+		OUTPUT+="\n"
+	    done
+	fi
 done
 
 GATEWAY=$(ip r | grep "default" | awk '{print $3}')
 
 #printf "$OUTPUT " '%s\n' "$GATEWAY"
-OUTPUT+="\nGateway:\t" 
+OUTPUT+="Gateway:\t" 
 OUTPUT+=$GATEWAY
 shopt -s xpg_echo
 
